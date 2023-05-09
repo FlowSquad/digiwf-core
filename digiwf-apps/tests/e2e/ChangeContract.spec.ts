58526e9aa2ec9d956cc60e777a133c64b08df36f
marber63 <martin.berner@miragon.io>
2023-05-09 16:05:27 +0200

    trials to get rid of keycloak cross origin issue

--- digiwf-apps/tests/e2e/ChangeContract.spec.ts
+++ digiwf-apps/tests/e2e/ChangeContract.spec.ts
@@ -12,12 +12,11 @@ describe("DIGIWF - Change Contract Testsuite", () => {
 
   it("#agt - create", () => {
     cy.log("WERWREWREWREWEWREW")
-    cy.visit("/#/mytask");
+    //cy.visit("/");
     // cy.origin("http://localhost:8082", () => {
     //   cy.visit("/#/mytask");
     //   // cy.log("WEWRWREWR");
-    cy.wait(120000);
-    cy.pause();
     // });
+    //cy.pause();
   });
 })
--- digiwf-apps/tests/support/commands.ts
+++ digiwf-apps/tests/support/commands.ts
@@ -4,32 +4,58 @@ import {
 
 Cypress.Commands.add("login", (url: string, user = "johndoe", pw = Cypress.env("KEYCLOAK_ADMIN")): Cypress.Chainable => {
   return cy.session([url, user, pw], () => {
-    const credentials = {username: user, password: pw};
+    const credentials = {username: user, password: pw, kc: KC};
+    let startPage = "";
     // blank page
     // get login form
-    cy.visit("/");
+    cy.visit("/")
     cy.get(KC.USERN).should("be.visible"); // wait for form loaded
-    // type credentials
-    cy.get(KC.USERN).type(credentials.username);
-    cy.get(KC.PASSW).type(credentials.password, {log: false});
-    // submit form
-    cy.pause();
-    //cy.get(KC.SUBMIT).click();
-    cy.get("#kc-form-login").submit();
-    cy.pause();
-     // cy.origin("http://localhost:8082", {args: credentials}, () => {
 
-      // cy.visit("/");
-       // cy.url().should("include", `${Cypress.config("baseUrl")}`);
-      // type credentials
-      // cy.get(KC.USERN).type(credentials.username);
-      // cy.get(KC.PASSW).type(credentials.password, {log: false});
-      // submit form
-      // cy.get(KC.SUBMIT).click();
-      // cy.request("GET", "http://keycloak:8080/auth/realms/P82").then((response) => {
-      // cy.log(response.body)
-   // });
-    //  });
+    // type credentials
+    cy.get(credentials.kc.USERN).type(credentials.username);
+    cy.get(credentials.kc.PASSW).type(credentials.password, {log: false});
+    // submit form
+    // cy.get(".aut-iframe").invoke("attr", "src").then((src) => {
+    //    cy.log(src);
+    // });
+    cy.pause();
+      cy.get(credentials.kc.SUBMIT).click();
+    // cy.pause();
+    // cy.log(startPage)
+     // cy.wait(5000);
+    // cy.origin("http://keycloak:8080", () => {
+  cy.visit("/");
+     cy.pause();
+    // });
+    // cy.origin("http://localhost:8082", {args: credentials}, (credentials) => {
+    //
+    //   //cy.getCookie("SESSION").then(cookie => {
+    //   //  cy.setCookie("SESSION", cookie.value);
+    //   //   cy.visit("/");
+    //   //   cy.pause();
+    //   // cy.visit("/");
+    //   cy.pause();
+    //   // });
+    // });
+
+    // cy.origin("http://keycloak:8080", {args: credentials}, (credentials) => {
+    //cy.origin("http://localhost:8082", () => {
+    // cy.visit("/auth/realms/P82");
+    // cy.get("#kc-form-login").submit();
+    // cy.pause();
+    // cy.origin("http://localhost:8082", {args: credentials}, () => {
+// ;
+    // cy.visit("/");
+    // cy.url().should("include", `${Cypress.config("baseUrl")}`);
+    // type credentials
+    // cy.get(KC.USERN).type(credentials.username);
+    // cy.get(KC.PASSW).type(credentials.password, {log: false});
+    // submit form
+    // cy.get(KC.SUBMIT).click();
+    //  cy.request("GET", "http://keycloak:8080/auth/realms/P82").then((response) => {
+    // cy.log(response.body)
+    // });
+    // });
     // cy.getAllCookies().then(cookies => {
     //     cy.log(JSON.stringify(cookies));
     // });
@@ -43,5 +69,6 @@ Cypress.Commands.add("login", (url: string, user = "johndoe", pw = Cypress.env("
     //cy.wait(3000);
     //cy.visit("/");
     // cy.log("WEWRWREWR");
+
   });
 });
