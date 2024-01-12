d438582879eba4a194ed918b79ddf4fceb859fca
Lukas Mösle <lukas95m@yahoo.de>
2024-01-12 16:21:48 +0100

    feature: remove spring cloud stream and add Rest controller

--- digiwf-integrations/document-signing-service/src/main/java/de/muenchen/oss/digiwf/signing/integration/application/port/CreateSigningUseCase.java
+++ digiwf-integrations/document-signing-service/src/main/java/de/muenchen/oss/digiwf/signing/integration/application/port/CreateSigningUseCase.java
@@ -1,13 +1,7 @@
 package de.muenchen.oss.digiwf.signing.integration.application.port;
 
-import de.muenchen.oss.digiwf.s3.integration.client.exception.DocumentStorageClientErrorException;
-import de.muenchen.oss.digiwf.s3.integration.client.exception.DocumentStorageException;
-import de.muenchen.oss.digiwf.s3.integration.client.exception.DocumentStorageServerErrorException;
-import de.muenchen.oss.digiwf.s3.integration.client.exception.PropertyNotSetException;
 import de.muenchen.oss.digiwf.signing.integration.application.port.in.CreateSigningInPort;
-import de.muenchen.oss.digiwf.signing.integration.application.port.out.DocumentStorageOutPort;
 import de.muenchen.oss.digiwf.signing.integration.application.port.out.SigningServiceOutPort;
-import de.muenchen.oss.digiwf.signing.integration.domain.model.CreateSigning;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,16 +9,10 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class CreateSigningUseCase implements CreateSigningInPort {
 
-    private final DocumentStorageOutPort documentStorageOutPort;
     private final SigningServiceOutPort signingServiceOutPort;
 
     @Override
-    public CreateSigning createSigning(String documentPath) throws DocumentStorageException, PropertyNotSetException, DocumentStorageClientErrorException, DocumentStorageServerErrorException {
-        final String storageUrl = documentStorageOutPort.createStorageUrl(documentPath);
-        final String signingUrl = signingServiceOutPort.createSigningUrl();
-        return CreateSigning.builder()
-                .storageUrl(storageUrl)
-                .signingUrl(signingUrl)
-                .build();
+    public String createSigning(String documentPath) {
+        return signingServiceOutPort.createSigningUrl();
     }
 }
--- digiwf-integrations/document-signing-service/src/main/java/de/muenchen/oss/digiwf/signing/integration/application/port/in/CreateSigningInPort.java
+++ digiwf-integrations/document-signing-service/src/main/java/de/muenchen/oss/digiwf/signing/integration/application/port/in/CreateSigningInPort.java
@@ -1,13 +1,7 @@
 package de.muenchen.oss.digiwf.signing.integration.application.port.in;
 
-import de.muenchen.oss.digiwf.s3.integration.client.exception.DocumentStorageClientErrorException;
-import de.muenchen.oss.digiwf.s3.integration.client.exception.DocumentStorageException;
-import de.muenchen.oss.digiwf.s3.integration.client.exception.DocumentStorageServerErrorException;
-import de.muenchen.oss.digiwf.s3.integration.client.exception.PropertyNotSetException;
-import de.muenchen.oss.digiwf.signing.integration.domain.model.CreateSigning;
-
 public interface CreateSigningInPort {
 
-    CreateSigning createSigning(String documentPath) throws DocumentStorageException, PropertyNotSetException, DocumentStorageClientErrorException, DocumentStorageServerErrorException;
+    String createSigning(String documentPath);
 
 }
